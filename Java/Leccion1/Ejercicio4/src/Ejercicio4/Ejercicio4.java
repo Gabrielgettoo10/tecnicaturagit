b85e6b67c2e75bcff3bbbf2ffb36445641fd32a6
Gabriel Garino <gabrielgarino10@gmail.com>
2024-06-25 23:34:38 -0300

    Se agrega todo

---
+++ Java/Leccion1/Ejercicio4/src/Ejercicio4/Ejercicio4.java
@@ -0,0 +1,17 @@
+package Ejercicio4;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite un número: ");
+        int num1 = Integer.parseInt(entrada.nextLine());
+        System.out.println("Digite otro número: ");
+        int num2 = Integer.parseInt(entrada.nextLine());
+        System.out.println("El número mayor es: ");
+        System.out.println(num1 > num2 ? num1 : num2);
+        System.out.println("");
+                      
+    }
+}
